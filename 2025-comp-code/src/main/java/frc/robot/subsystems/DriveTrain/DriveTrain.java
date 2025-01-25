869fb076dd5ec760fa60faae2252f47e5cfb750f
ElsyFM <146791282+ElsyFM@users.noreply.github.com>
2025-01-25 13:20:17 -0500

    it runs :p

--- 2025-comp-code/src/main/java/frc/robot/subsystems/DriveTrain/DriveTrain.java
+++ 2025-comp-code/src/main/java/frc/robot/subsystems/DriveTrain/DriveTrain.java
@@ -1,9 +1,6 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.DriveTrain;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.SparkAbsoluteEncoder.Type;
-import com.revrobotics.SparkPIDController;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class DriveTrain {
--- 2025-comp-code/src/main/java/frc/robot/subsystems/DriveTrain/SwerveModules.java
+++ 2025-comp-code/src/main/java/frc/robot/subsystems/DriveTrain/SwerveModules.java
@@ -3,13 +3,18 @@ package frc.robot.subsystems.DriveTrain;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkClosedLoopController;
+
+import java.io.ObjectInputFilter.Config;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class SwerveModules {    
     
-    private SparkMax sparkMotorTest;
-
-    sparkMotorTest.
+    private SparkMax max = new SparkMax(1, MotorType.kBrushless);
+    private SparkMaxConfig config = new SparkMaxConfig();
+   
 }
+
