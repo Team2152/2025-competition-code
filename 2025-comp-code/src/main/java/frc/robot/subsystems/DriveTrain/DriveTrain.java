9692eb4a651fb292b2033ff31c049d22539b4579
ElsyFM <146791282+ElsyFM@users.noreply.github.com>
2025-01-25 11:29:34 -0500

    lib

---
+++ 2025-comp-code/src/main/java/frc/robot/subsystems/DriveTrain/DriveTrain.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.revrobotics.SparkPIDController;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class DriveTrain {
+    
+}
---
+++ 2025-comp-code/src/main/java/frc/robot/subsystems/DriveTrain/SwerveModules.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.revrobotics.SparkPIDController;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class SwerveModules {
+
+    private final TalonFx 
+    
+}
